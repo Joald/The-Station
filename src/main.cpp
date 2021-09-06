8d4b5c8bc360c542bef8ac2b35be58d662a7f75b
Jacek Olczyk <jacek.olczyk98@gmail.com>
2021-09-06 20:51:03 +0200

    Make creation of collision graph more readable

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,28 @@ int main() {
     debugAssert(window.isOpen(), "Could not open window.");
     window.add({player, 1});
     EventManager::registerSfmlEvent(
-      sf::Event::Closed, [&](sf::Event event) {
-          debugLog("Closed!");
-          window.close();
-      }
+            sf::Event::Closed, [&](sf::Event event) {
+                debugLog("Closed!");
+                window.close();
+            }
     );
     EventManager::registerSfmlEvent(
-      sf::Event::KeyPressed, [&](sf::Event event) {
-          if (event.key.code == sf::Keyboard::Escape) {
-              window.close();
-          }
-      }
+            sf::Event::KeyPressed, [&](sf::Event event) {
+                if (event.key.code == sf::Keyboard::Escape) {
+                    window.close();
+                }
+            }
     );
+
+    std::vector<CollisionGraph::Edge> edges = {
+            std::make_pair(std::string(player->getName()), "screenEdge"),
+    };
+
+    auto graph = std::make_shared<CollisionGraph>(std::move(edges));
+
     STEngine::collisionDetectors().emplace(
-      player->getName(),
-      std::make_shared<CollisionGraph>(
-        std::vector<CollisionGraph::Edge>(1, std::make_pair(std::string(player->getName()), "screenEdge"))
-      )
+            player->getName(),
+            std::move(graph)
     );
 
     player->setPosition(200, 200);
