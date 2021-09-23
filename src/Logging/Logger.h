5e7862cca19a1024cd22952048fe3a088b8048f2
Jacek Olczyk <jacek.olczyk98@gmail.com>
2021-09-23 14:00:00 +0200

    Improve logger construction

--- src/Logging/Logger.h
+++ src/Logging/Logger.h
@@ -10,12 +10,13 @@
 #include <chrono>
 #include <iomanip>
 #include <filesystem>
+#include <optional>
 
 class Logger {
     const inline static bool DEBUG = true;
-    const std::string filename;
 
-    std::fstream fstream;
+    const std::string_view filename;
+    std::optional<std::ofstream> ofstream;
     std::ostream* stream;
 
     class LoggerHelper {
@@ -37,15 +38,11 @@ class Logger {
 
 
 public:
-    explicit Logger(std::string_view fileName = "") :
-            filename(fileName), fstream() {
-        if (!filename.empty()) {
-            fstream.open(filename);
-            stream = &fstream;
-        } else {
-            stream = &std::cerr;
-        }
-    }
+    Logger() noexcept : stream(&std::cerr) {}
+    explicit Logger(std::string_view fileName) :
+            filename(fileName),
+            ofstream(std::ofstream{fileName.data()}),
+            stream(&ofstream.value()) {}
 
     LoggerHelper operator()(std::source_location loc = std::source_location::current());
 
